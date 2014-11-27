6627fc9b714df03acad29a5af96da10a527d83c4
rajeshy2 <yashaswinirajesh@gmail.com>
2014-11-26 16:19:53 -0800

    no changes

--- src/com/msd/dam.java
+++ src/com/msd/dam.java
@@ -6,9 +6,9 @@ public class dam
     * static main : String[] -> void
     * @param args : The command line arguments
     * Effect: The program starts execution from this method.
-    *         If the 'args' is a valid command compares the two files
-    *         else prints message through standard error and exits with 
-    *         status other than 0
+    *         If the 'args' is a valid command, the program compares the
+    *         two files else prints message through standard error and 
+    *         exits with status other than 0
     */
    public static void main(String[] args)
    {
